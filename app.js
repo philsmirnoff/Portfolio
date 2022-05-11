2aa1471a5963242147e43a30a6085dc30ec56583
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-05-10 23:57:08 -0400

    working on adding fade in effect

--- app.js
+++ app.js
@@ -25,3 +25,13 @@ menu_item.forEach((item) => {
 	});
 
 });
+
+// fade in effects
+$(document).ready(function() {
+$('.js--wp-1').waypoint(function(direction) {
+	$('.js--wp-1').addClass('animated fadeIn');
+}, {
+	offset: '50%'
+});
+
+});
