a95e692e1b96188b390a520129f7715d3e84249a
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-06-09 23:22:47 -0400

    updated the project

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+function reveal() {
+  var reveals = document.querySelectorAll("#about");
+
+	for (var i = 0; i < reveals.length; i++) {
+		var windowHeight = window.innerHeight;
+		var elementTop = reveals[i].getBoundingClientRect().top;
+		var elementVisible = 150;
+		if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+	}
+}
+window.addEventListener("scroll", reveal);
+
 hamburger.addEventListener('click',() => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
@@ -28,8 +44,8 @@ menu_item.forEach((item) => {
 
 // fade in effects
 $(document).ready(function() {
-$('.js--wp-1').waypoint(function(direction) {
-	$('.js--wp-1').addClass('animated fadeIn');
+$('.col-right').waypoint(function(direction) {
+	$('.col-right').addClass('animated fadeIn');
 }, {
 	offset: '50%'
 });
